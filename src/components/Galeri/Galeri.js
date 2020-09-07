a70d485edf75ce7a1db5455853f1fb4aa69d7963
Emre Burak Baş <44948126+Emreburak1@users.noreply.github.com>
2020-09-07 11:20:09 +0300

    Update Galeri.js

--- src/components/Galeri/Galeri.js
+++ src/components/Galeri/Galeri.js
@@ -1,44 +1,46 @@
-import React, {useState, useEffect} from 'react';
-import ImgThumbnail from './imgThumbnail'
-import axios from 'axios'
-// import { GaleriItems } from './galeri_items' 
+import React, { useState, useEffect } from "react";
+import ImgThumbnail from "./imgThumbnail";
+const contentful = require("contentful");
 
 function Galeri() {
+	const client = contentful.createClient({
+		space: "pfpwc05vxh8u",
+		accessToken: "WeygON6Z-mco0Q-A-smOEHfbn3Stq7bqwMTSSFyYpCE",
+	});
 
-	const [galeriItems,setGaleriItems] = useState([]);
+	const [galeriItems, setGaleriItems] = useState([]);
 
 	useEffect(() => {
 		console.log("effect");
-		
-		axios.get("https://lciepy4cof.execute-api.us-east-1.amazonaws.com/gallery")
-		
-		.then((response) => {
-			console.log("promise fulfilled");
-			console.log(response)
-			setGaleriItems(response.data[0].fields.list);
-		});
+
+		client
+			.getEntries({
+				content_type: "gallery",
+			})
+
+			.then((response) => {
+				console.log("promise fulfilled");
+				console.log(response.items[0].fields.list);
+				setGaleriItems(response.items[0].fields.list);
+			});
 	}, []);
 
 	console.log("render", galeriItems.length, "notes");
-	console.log(galeriItems)
-	
-
+	console.log(galeriItems);
 
 	return (
 		<div class="container">
-
 			<h1 class="text-center text-lg-left mt-4 mb-0">Fotoğraflarda Compec</h1>
 
 			<hr class="mt-2 mb-5" />
 
 			<div class="row text-center text-lg-left">
-
-				{
-					galeriItems.map(
-						(item) => <ImgThumbnail src={item.fields.thumbnail.fields.file.url} link={item.fields.image.fields.file.url} />
-					)		
-				}
-
+				{galeriItems.map((item) => (
+					<ImgThumbnail
+						src={item.fields.thumbnail.fields.file.url}
+						link={item.fields.image.fields.file.url}
+					/>
+				))}
 			</div>
 		</div>
 	);
